0dc28b260012f4b0b792c8cae9e67fc2270cc4be
hwxuan <1325997218@qq.com>
2024-06-30 17:33:12 +0800

    add many git

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-git init
+git：命令行输入git，可以查看系统有没有安装Git
+git config --global user.name "xuanhongwei"
+git config --global user.email "1325997218@qq.com"
+git init：把目录变成Git可以管理的仓库
+git add test.py：把文件添加到仓库
+git commit -m "add XXX"：把文件提交到仓库
 git status：随时掌握工作区的状态
 git diff：如果git status告知有文件被修改过，用git diff可以查看修改的内容
