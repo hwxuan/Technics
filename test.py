76bf34988fed2342955cc9f7631b398dd8dd3b89
hwxuan <1325997218@qq.com>
2024-07-07 14:12:20 +0800

    commit

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ git add test.py：把文件添加到暂存区
 git commit -m "add XXX"：把文件提交到版本库(仓库)
 git status：随时掌握工作区的状态
 git diff：如果git status告知有文件被修改过，用git diff可以查看修改的内容
-git log：穿梭历史前，用此命令查看提交历史，以便确定要回退到哪个版本，添加--pretty oneline，精简显示
+git log：穿梭历史前，用此命令查看提交历史，以便确定要回退到哪个版本，添加--pretty=oneline，精简显示
 git reflog：重返未来前，用此命令查看命令历史，以便确定要回到未来的哪个版本
 git reset --hard commit_id：使用此命令，在历史版本间穿梭
 
